04430ae82697776db7e17fabf618aa308ed8a7a5
wellwashwork <wellwashwork@gmail.com>
2022-05-15 15:12:17 +0530

    first commit

---
+++ src/sections/@dashboard/e-commerce/ProductNewEditForm.js
@@ -0,0 +1,194 @@
+import PropTypes from 'prop-types';
+import * as Yup from 'yup';
+import { useSnackbar } from 'notistack';
+import { useNavigate } from 'react-router-dom';
+import { useCallback, useEffect, useMemo } from 'react';
+// form
+import { useForm, Controller } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
+// @mui
+import { styled } from '@mui/material/styles';
+import { LoadingButton } from '@mui/lab';
+import { Card, Chip, Grid, Stack, TextField, Typography, Autocomplete, InputAdornment } from '@mui/material';
+// routes
+import { PATH_DASHBOARD } from '../../../routes/paths';
+// components
+import {
+  FormProvider,
+  RHFSwitch,
+  RHFSelect,
+  RHFEditor,
+  RHFTextField,
+  RHFRadioGroup,
+  RHFUploadMultiFile,
+} from '../../../components/hook-form';
+
+// ----------------------------------------------------------------------
+
+const GENDER_OPTION = [
+  { label: 'Men', value: 'Men' },
+  { label: 'Women', value: 'Women' },
+  { label: 'Kids', value: 'Kids' },
+];
+
+const CATEGORY_OPTION = [
+  { group: 'Clothing', classify: ['Shirts', 'T-shirts', 'Jeans', 'Leather'] },
+  { group: 'Tailored', classify: ['Suits', 'Blazers', 'Trousers', 'Waistcoats'] },
+  { group: 'Accessories', classify: ['Shoes', 'Backpacks and bags', 'Bracelets', 'Face masks'] },
+];
+
+const TAGS_OPTION = [
+  'Toy Story 3',
+  'Logan',
+  'Full Metal Jacket',
+  'Dangal',
+  'The Sting',
+  '2001: A Space Odyssey',
+  "Singin' in the Rain",
+  'Toy Story',
+  'Bicycle Thieves',
+  'The Kid',
+  'Inglourious Basterds',
+  'Snatch',
+  '3 Idiots',
+];
+
+const LabelStyle = styled(Typography)(({ theme }) => ({
+  ...theme.typography.subtitle2,
+  color: theme.palette.text.secondary,
+  marginBottom: theme.spacing(1),
+}));
+
+// ----------------------------------------------------------------------
+
+ProductNewEditForm.propTypes = {
+  isEdit: PropTypes.bool,
+  currentProduct: PropTypes.object,
+};
+
+export default function ProductNewEditForm({ isEdit, currentProduct }) {
+  const navigate = useNavigate();
+
+  const { enqueueSnackbar } = useSnackbar();
+
+  const NewProductSchema = Yup.object().shape({
+    name: Yup.string().required('Name is required'),
+    description: Yup.string().required('Description is required'),
+    images: Yup.array().min(1, 'Images is required'),
+    price: Yup.number().moreThan(0, 'Price should not be $0.00'),
+  });
+
+  const defaultValues = useMemo(
+    () => ({
+      name: currentProduct?.name || '',
+      description: currentProduct?.description || '',
+      images: currentProduct?.images || [],
+      code: currentProduct?.code || '',
+      sku: currentProduct?.sku || '',
+      price: currentProduct?.price || 0,
+      priceSale: currentProduct?.priceSale || 0,
+      tags: currentProduct?.tags || [TAGS_OPTION[0]],
+      inStock: true,
+      taxes: true,
+      gender: currentProduct?.gender || GENDER_OPTION[2].value,
+      category: currentProduct?.category || CATEGORY_OPTION[0].classify[1],
+    }),
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    [currentProduct]
+  );
+
+  const methods = useForm({
+    resolver: yupResolver(NewProductSchema),
+    defaultValues,
+  });
+
+  const {
+    reset,
+    watch,
+    control,
+    setValue,
+    getValues,
+    handleSubmit,
+    formState: { isSubmitting },
+  } = methods;
+
+  const values = watch();
+
+  useEffect(() => {
+    if (isEdit && currentProduct) {
+      reset(defaultValues);
+    }
+    if (!isEdit) {
+      reset(defaultValues);
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [isEdit, currentProduct]);
+
+  const onSubmit = async () => {
+    try {
+      await new Promise((resolve) => setTimeout(resolve, 500));
+      reset();
+      enqueueSnackbar(!isEdit ? 'Upload success!' : 'Update success!');
+      navigate(PATH_DASHBOARD.eCommerce.list);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const handleDrop = useCallback(
+    (acceptedFiles) => {
+      const images = values.images || [];
+
+      setValue('images', [
+        ...images,
+        ...acceptedFiles.map((file) =>
+          Object.assign(file, {
+            preview: URL.createObjectURL(file),
+          })
+        ),
+      ]);
+    },
+    [setValue, values.images]
+  );
+
+  const handleRemoveAll = () => {
+    setValue('images', []);
+  };
+
+  const handleRemove = (file) => {
+    const filteredItems = values.images?.filter((_file) => _file !== file);
+    setValue('images', filteredItems);
+  };
+
+  return (
+    <FormProvider methods={methods} onSubmit={handleSubmit(onSubmit)}>
+      <Grid container spacing={3}>
+        <Grid item xs={12} md={8}>
+          <Card sx={{ p: 3 }}>
+            <Stack spacing={3}>
+              <div>
+                <LabelStyle>Upload Files</LabelStyle>
+                <RHFUploadMultiFile
+                  showPreview
+                  name="images"
+                  accept="image/*"
+                  maxSize={3145728}
+                  onDrop={handleDrop}
+                  onRemove={handleRemove}
+                  onRemoveAll={handleRemoveAll}
+                  onUpload={() => console.log('ON UPLOAD')}
+                />
+              </div>
+            </Stack>
+          </Card>
+
+          <Stack spacing={3}>
+            <LoadingButton type="submit" variant="contained" size="large" loading={isSubmitting}>
+              {!isEdit ? 'Upload' : 'Save Changes'}
+            </LoadingButton>
+          </Stack>
+        </Grid>
+      </Grid>
+    </FormProvider>
+  );
+}
